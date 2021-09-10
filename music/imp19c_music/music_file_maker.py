63e350875990a556795abe876d57a3dfe9b63137
Sobisonator <bomchasew@gmail.com>
2021-09-10 21:54:42 +0100

    Fixed music file generation Added more music

--- music/imp19c_music/music_file_maker.py
+++ music/imp19c_music/music_file_maker.py
@@ -1,12 +1,12 @@
 import glob
 music_files = glob.glob("*.mp3")
 
-track_format = 'track%(trackno)x = { \n    music = "file:/music/imp19c_music/%(trackname)s" \n    weight = 25 \n    mood = yes \n    can_be_interrupted = yes \n}\n'
+track_format = 'track%(trackno)d = { \n    music = "file:/music/imp19c_music/%(trackname)s" \n    weight = 25 \n    mood = yes \n    can_be_interrupted = yes \n}\n'
 
 def write_music(music_files):
    with open ("music.txt","w") as music_txt:
-       i = 1
-       for file in music_files:
+      i = 1
+      for file in music_files:
            music_txt.write(track_format % {"trackno": i, "trackname": file})
            i = i+1
                            

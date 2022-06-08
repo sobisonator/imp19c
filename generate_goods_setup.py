4b4ef63778c71d661fbc6009bb3d7204ce893584
Sobisonator <bomchasew@gmail.com>
2022-06-08 22:58:59 +0100

    3 new industries, 2 replaced Replaced paper with glass Replaced household wares with glass Added aeroplanes

--- generate_goods_setup.py
+++ generate_goods_setup.py
@@ -7,7 +7,7 @@ def print_out_good(goods_name):
 	)""".format(goods_name=goods_name)
 	print(loc)
 
-all_goods = ["clothing","luxury_clothing","furniture","luxury_furniture","alcohol","household_wares","chemicals","rare_alloys","construction_materials","early_munitions","late_munitions","naval_supplies","steel_ships","wooden_ships","steel","bronze","machine_parts","early_artillery","late_artillery","paper","pharmaceuticals","motors","processed_foods"]
+all_goods = ["clothing","luxury_clothing","furniture","luxury_furniture","alcohol","glass","chemicals","rare_alloys","construction_materials","early_munitions","late_munitions","naval_supplies","steel_ships","wooden_ships","steel","bronze","machine_parts","early_artillery","late_artillery","electronics","pharmaceuticals","motors","processed_foods"]
 
 for good in all_goods:
 	print_out_good(good)
--- localization/english/generate_goods_tooltips.py
+++ localization/english/generate_goods_tooltips.py
@@ -7,7 +7,7 @@ PROVWINDOW_GOV_{caps_name}_NUM_FACTORIES:0 "x[GuiScope.SetRoot(ProvinceWindow.Ge
 PROVWINDOW_GOV_{caps_name}_PRODUCED_TT:0 "#T {initial_cap_name} produced in [ProvinceWindow.GetState.GetGovernorship.GetName]#!: [GuiScope.SetRoot(ProvinceWindow.GetState.GetGovernorship.MakeScope).ScriptValue('GOODS_governorship_{lowercase_name}_produced')|0]\n""""".format(caps_name=caps_name,lowercase_name=lowercase_name,initial_cap_name=initial_cap_name)
 	print(loc)
 
-all_goods = ["clothing","luxury_clothing","furniture","luxury_furniture","alcohol","household_wares","chemicals","rare_alloys","construction_materials","early_munitions","late_munitions","naval_supplies","steel_ships","wooden_ships","steel","bronze","machine_parts","early_artillery","late_artillery","paper","pharmaceuticals","motors","processed_foods"]
+all_goods = ["clothing","luxury_clothing","furniture","luxury_furniture","alcohol","glass","chemicals","rare_alloys","construction_materials","early_munitions","late_munitions","naval_supplies","steel_ships","wooden_ships","steel","bronze","machine_parts","early_artillery","late_artillery","electronics","pharmaceuticals","motors","processed_foods","aeroplanes"]
 
 for good in all_goods:
 	print_out_good(good)

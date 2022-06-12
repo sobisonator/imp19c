f6f065784810caf64af5ca75ea10b05c1e85d3fa
Sobisonator <bomchasew@gmail.com>
2022-06-13 00:30:45 +0100

    Refactored MovementValues.py to print directly to file

--- zz_TradeZoneScript/MovementValues.py
+++ zz_TradeZoneScript/MovementValues.py
@@ -1,23 +1,48 @@
 import TradeZones #Import the trade zones dictionary and list.
 
+f = open("movementvalues_output.txt","w")
+
 def print_trade_zone_header(trade_zone):
 	caps_name = trade_zone.replace("_", " ").title()
 	trade_zone_header = "\n#---------------------------------------\n#Trade Zone: {caps_name}\n#---------------------------------------\n".format(caps_name=caps_name)
-	print(trade_zone_header)
+	print(trade_zone_header,file=f)
 
 def print_trade_zone_connection_values(trade_zone, trade_zone_2):
 	zone_1 = trade_zone.lower()
 	zone_2 = trade_zone_2.lower()
 	trade_zone_connections = "{zone_1}_to_{zone_2}_svalue = {{\n    value = 100 #Base Value\n    subtract = {zone_1}_transportation_svalue\n    subtract = {zone_2}_transportation_svalue\n}}".format(zone_1=zone_1,zone_2=zone_2)
-	print(trade_zone_connections)
+	print(trade_zone_connections,file=f)
 
 def print_movement_script_values(trade_zone):
 	print_trade_zone_header(trade_zone)
-	print("{trade_zone}_transportation_svalue = {{\n    value = 0".format(trade_zone=trade_zone))
+	print("{trade_zone}_transportation_svalue = {{\n    value = 0".format(trade_zone=trade_zone),file=f)
 	for x in TradeZones.trade_zones_dict.get(trade_zone):
 		for i in x:
-			print("    region:{i} = {{\n        if = {{\n            limit = {{\n                any_region_province = {{\n                    OR = {{\n                        any_neighbor_province = {{ has_road_towards = PREV }}\n                        has_building = port_building\n                    }}\n                }}\n            }}\n            every_region_province = {{\n                limit = {{\n                    any_neighbor_province = {{ has_road_towards = PREV }}\n                }}\n                add = railway_bonus\n            }}\n            every_region_province = {{\n                limit = {{\n                    has_building = port_building\n                }}\n                add = port_bonus\n            }}\n        }}\n    }}".format(i=i))
-	print("}")
+			print("""    region:{i} = {{
+			if = {{            
+				limit = {{               
+					any_region_province = {{                   
+						OR = {{                       
+							any_neighbor_province = {{ has_road_towards = PREV }}                       
+							has_building = port_building                   
+						}}               
+					}}            
+				}}            
+				every_region_province = {{                
+					limit = {{                    
+						any_neighbor_province = {{ has_road_towards = PREV }}                
+						}}                
+					add = railway_bonus            
+				}}\n            
+				every_region_province = {{                
+					limit = {{                    
+						has_building = port_building\n                
+					}}                
+				add = port_bonus           
+			}}        
+		}}    
+	}}""".format(i=i),file=f)
+	print("}",file=f)
 
 def connection_values(trade_zone_list):
 	for zone in trade_zone_list:

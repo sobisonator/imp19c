de9a2694c87f7e0cf0f00b7f788b333a7ef30ff6
Sobisonator <bomchasew@gmail.com>
2022-10-17 23:36:38 +0100

    Fixed the internal cuts of revenue missing trade goods other than grain

--- tradegood_repeater.py
+++ tradegood_repeater.py
@@ -1,9 +1,10 @@
 def print_out_good(goods_name, category_name):
 	goods_name = goods_name
 	category_name = category_name
-	loc = """DEMAND_{goods_name} = {{ # Total
-	value = DEMAND_tiny # placeholder
-}}""".format(goods_name=goods_name,category_name=category_name)
+	loc = """	set_variable = {{
+		name = internal_stockpile_cut_percentage_{goods_name}
+		value = 0
+	}}""".format(goods_name=goods_name,category_name=category_name)
 	print(loc)
 
 #all_goods = ["grain","fur","industrial_fibres","textile_fibres","wool","silk","wood","stone","sulphur","whales","gems","peat","tin","inorganic_compounds","copper","iron","gold","silver","lead","coal","oil","tea","coffee","opium","tobacco","sugar","hardwood","rubber","dye","spices","temperate_fruit","tropical_fruit","mediterranean_fruit","chocolate","livestock","salt","fish","clothing","luxury_clothing","furniture","luxury_furniture","alcohol","glass","chemicals","rare_alloys","construction_materials","early_munitions","late_munitions","naval_supplies","steel_ships","wooden_ships","steel","bronze","machine_parts","early_artillery","late_artillery","electronics","pharmaceuticals","motors","processed_foods","petrochemicals"]

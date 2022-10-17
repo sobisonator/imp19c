d2f40fa73749b9b81c9b6b18ceed5200057b73d3
Sobisonator <bomchasew@gmail.com>
2022-10-17 22:52:31 +0100

    Added placeholder demand svalues for all tradegoods

--- tradegood_repeater.py
+++ tradegood_repeater.py
@@ -1,12 +1,9 @@
 def print_out_good(goods_name, category_name):
 	goods_name = goods_name
 	category_name = category_name
-	loc = """	every_country = {{
-		TRADE_reset_internal_amount_sold = {{ tradegood = {goods_name} }}
-	}}
-	every_federation = {{
-		TRADE_reset_internal_amount_sold = {{ tradegood = {goods_name} }}
-	}}""".format(goods_name=goods_name,category_name=category_name)
+	loc = """DEMAND_{goods_name} = {{ # Total
+	value = DEMAND_tiny # placeholder
+}}""".format(goods_name=goods_name,category_name=category_name)
 	print(loc)
 
 #all_goods = ["grain","fur","industrial_fibres","textile_fibres","wool","silk","wood","stone","sulphur","whales","gems","peat","tin","inorganic_compounds","copper","iron","gold","silver","lead","coal","oil","tea","coffee","opium","tobacco","sugar","hardwood","rubber","dye","spices","temperate_fruit","tropical_fruit","mediterranean_fruit","chocolate","livestock","salt","fish","clothing","luxury_clothing","furniture","luxury_furniture","alcohol","glass","chemicals","rare_alloys","construction_materials","early_munitions","late_munitions","naval_supplies","steel_ships","wooden_ships","steel","bronze","machine_parts","early_artillery","late_artillery","electronics","pharmaceuticals","motors","processed_foods","petrochemicals"]

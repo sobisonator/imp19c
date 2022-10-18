c2abf1c135fb616a0ecf0bc19122c37521f20997
Sobisonator <bomchasew@gmail.com>
2022-10-18 01:00:02 +0100

    Added missing trade svalues for most tradegoods

--- tradegood_repeater.py
+++ tradegood_repeater.py
@@ -1,10 +1,10 @@
 def print_out_good(goods_name, category_name):
 	goods_name = goods_name
 	category_name = category_name
-	loc = """	set_variable = {{
-		name = internal_stockpile_cut_percentage_{goods_name}
-		value = 0
-	}}""".format(goods_name=goods_name,category_name=category_name)
+	loc = """TRADE_governorship_export_cap_{goods_name} = {{ # The upper limit over which a governorship is NOT allowed to export any of this good. Should be derived from a variable
+# FOR NOW, THIS IS JUST A TEST AND HAS NO VARIABLE FROM WHICH IT DERIVES
+	value = 100
+}}\n""".format(goods_name=goods_name,category_name=category_name)
 	print(loc)
 
 #all_goods = ["grain","fur","industrial_fibres","textile_fibres","wool","silk","wood","stone","sulphur","whales","gems","peat","tin","inorganic_compounds","copper","iron","gold","silver","lead","coal","oil","tea","coffee","opium","tobacco","sugar","hardwood","rubber","dye","spices","temperate_fruit","tropical_fruit","mediterranean_fruit","chocolate","livestock","salt","fish","clothing","luxury_clothing","furniture","luxury_furniture","alcohol","glass","chemicals","rare_alloys","construction_materials","early_munitions","late_munitions","naval_supplies","steel_ships","wooden_ships","steel","bronze","machine_parts","early_artillery","late_artillery","electronics","pharmaceuticals","motors","processed_foods","petrochemicals"]

075a2e59d9f5d4a76ed26d718bc29c6c9f8305dc
Sobisonator <bomchasew@gmail.com>
2023-01-27 19:57:35 +0000

    WiP saving inter-TZ distances as variables in trade center scopes

--- TZ_repeater.py
+++ TZ_repeater.py
@@ -1,10 +1,12 @@
 def print_TZ_statement(TZ):
-	loc = """else_if = {{
-	limit = {{ has_variable = {TZ} }}
-	PURCHASE_rank_supplier_zone_internal = {{
-                tradezone = {TZ}
-        }}
-}}""".format(TZ=TZ)
+	loc = """		else_if = {{
+			limit = {{
+				has_variable = {{ {TZ} }}
+			}}
+			set_connection_time_vars = {{
+				tradezone = {TZ}
+			}}
+		}}""".format(TZ=TZ)
 	print(loc)
 
 #all_goods = ["grain","fur","industrial_fibres","textile_fibres","wool","silk","wood","stone","sulphur","whales","gems","peat","tin","inorganic_compounds","copper","iron","gold","silver","lead","coal","oil","tea","coffee","opium","tobacco","sugar","hardwood","rubber","dye","spices","temperate_fruit","tropical_fruit","mediterranean_fruit","chocolate","livestock","salt","fish","clothing","luxury_clothing","furniture","luxury_furniture","alcohol","glass","chemicals","rare_alloys","construction_materials","early_munitions","late_munitions","naval_supplies","steel_ships","wooden_ships","steel","bronze","machine_parts","early_artillery","late_artillery","electronics","pharmaceuticals","motors","processed_foods","petrochemicals"]

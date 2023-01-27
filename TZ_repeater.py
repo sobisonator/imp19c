7d8e05b13322aa7810908b4fb5299e63bf9da4aa
Sobisonator <bomchasew@gmail.com>
2023-01-27 01:31:35 +0000

    Rough tradezone purchase ranking script draft

--- TZ_repeater.py
+++ TZ_repeater.py
@@ -1,33 +1,35 @@
 def print_TZ_statement(TZ):
-	loc = """	set_global_variable = {{
-		name = $tradezone$_to_{TZ}
-		value = $tradezone$_to_{TZ}_svalue
-	}}""".format(TZ=TZ)
+	loc = """else_if = {{
+	limit = {{ has_variable = {TZ} }}
+	PURCHASE_rank_supplier_zone_internal = {{
+                tradezone = {TZ}
+        }}
+}}""".format(TZ=TZ)
 	print(loc)
 
 #all_goods = ["grain","fur","industrial_fibres","textile_fibres","wool","silk","wood","stone","sulphur","whales","gems","peat","tin","inorganic_compounds","copper","iron","gold","silver","lead","coal","oil","tea","coffee","opium","tobacco","sugar","hardwood","rubber","dye","spices","temperate_fruit","tropical_fruit","mediterranean_fruit","chocolate","livestock","salt","fish","clothing","luxury_clothing","furniture","luxury_furniture","alcohol","glass","chemicals","rare_alloys","construction_materials","early_munitions","late_munitions","naval_supplies","steel_ships","wooden_ships","steel","bronze","machine_parts","early_artillery","late_artillery","electronics","pharmaceuticals","motors","processed_foods","petrochemicals"]
-all_TZs = ["india",
-           "east_north_america",
-           "west_north_america",
-           "caribbean",
-           "west_south_america",
-           "east_south_america",
-           "south_east_asia",
-           "indo_china",
-           "yellow_sea",
-           "southern_africa",
-           "west_africa",
-           "east_africa",
-           "middle_east",
-           "western_steppe",
-           "eastern_steppe",
-           "upper_yangtzi",
-           "atlantic_seaboard",
-           "central_europe",
-           "west_mediterranean",
-           "baltic",
-           "east_europe",
-           "east_mediterranean"]
+all_TZs = ["india_tradezone",
+           "east_north_america_tradezone",
+           "west_north_america_tradezone",
+           "caribbean_tradezone",
+           "west_south_america_tradezone",
+           "east_south_america_tradezone",
+           "south_east_asia_tradezone",
+           "indo_china_tradezone",
+           "yellow_sea_tradezone",
+           "southern_africa_tradezone",
+           "west_africa_tradezone",
+           "east_africa_tradezone",
+           "middle_east_tradezone",
+           "western_steppe_tradezone",
+           "eastern_steppe_tradezone",
+           "upper_yangtzi_tradezone",
+           "atlantic_seaboard_tradezone",
+           "central_europe_tradezone",
+           "west_mediterranean_tradezone",
+           "baltic_tradezone",
+           "east_europe_tradezone",
+           "east_mediterranean_tradezone"]
 
 for TZ in all_TZs:
         print_TZ_statement(TZ = TZ)

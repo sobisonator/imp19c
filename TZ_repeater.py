df80e44564006c7c654afca671bf4ce129fadbdf
Sobisonator <bomchasew@gmail.com>
2023-01-28 04:06:13 +0000

    WiP - Added functionality for purchasing tradezones to rank suppliers This has no effect yet New issues: Does not account for nonexisting/empty TZ stockpiles, so throws errors Failed to fetch variable for 'early_artillery_stockpile_east_mediterranean_tradezone' due to not being set
    PURCHASE_order_internal needs to be changed so that the i_demand_diff checks the supplier TZ. Ideally this is moved into the rank suppliers, so that purchasers only rank suppliers who can fulfil the order.

--- TZ_repeater.py
+++ TZ_repeater.py
@@ -1,37 +1,41 @@
 def print_TZ_statement(TZ):
-	loc = """		else_if = {{
-			limit = {{
-				has_variable = {{ {TZ} }}
+	loc = """	else_if = {{
+		limit = {{
+			var:purhcaser_TZ = {{
+				has_variable = is_{TZ}_tradezone
 			}}
-			set_connection_time_vars = {{
-				tradezone = {TZ}
-			}}
-		}}""".format(TZ=TZ)
+		}}
+		PURHCASE_get_seller_purchaser_distance_score = {{
+			tradegood = $tradegood$
+			tradezone = $tradezone$
+			purchaser_TZ = {TZ}
+		}}
+	}}""".format(TZ=TZ)
 	print(loc)
 
 #all_goods = ["grain","fur","industrial_fibres","textile_fibres","wool","silk","wood","stone","sulphur","whales","gems","peat","tin","inorganic_compounds","copper","iron","gold","silver","lead","coal","oil","tea","coffee","opium","tobacco","sugar","hardwood","rubber","dye","spices","temperate_fruit","tropical_fruit","mediterranean_fruit","chocolate","livestock","salt","fish","clothing","luxury_clothing","furniture","luxury_furniture","alcohol","glass","chemicals","rare_alloys","construction_materials","early_munitions","late_munitions","naval_supplies","steel_ships","wooden_ships","steel","bronze","machine_parts","early_artillery","late_artillery","electronics","pharmaceuticals","motors","processed_foods","petrochemicals"]
-all_TZs = ["india_tradezone",
-           "east_north_america_tradezone",
-           "west_north_america_tradezone",
-           "caribbean_tradezone",
-           "west_south_america_tradezone",
-           "east_south_america_tradezone",
-           "south_east_asia_tradezone",
-           "indo_china_tradezone",
-           "yellow_sea_tradezone",
-           "southern_africa_tradezone",
-           "west_africa_tradezone",
-           "east_africa_tradezone",
-           "middle_east_tradezone",
-           "western_steppe_tradezone",
-           "eastern_steppe_tradezone",
-           "upper_yangtzi_tradezone",
-           "atlantic_seaboard_tradezone",
-           "central_europe_tradezone",
-           "west_mediterranean_tradezone",
-           "baltic_tradezone",
-           "east_europe_tradezone",
-           "east_mediterranean_tradezone"]
+all_TZs = ["india",
+           "east_north_america",
+           "west_north_america",
+           "caribbean",
+           "west_south_america",
+           "east_south_america",
+           "south_east_asia",
+           "indo_china",
+           "yellow_sea",
+           "southern_africa",
+           "west_africa",
+           "east_africa",
+           "middle_east",
+           "western_steppe",
+           "eastern_steppe",
+           "upper_yangtzi",
+           "atlantic_seaboard",
+           "central_europe",
+           "west_mediterranean",
+           "baltic",
+           "east_europe",
+           "east_mediterranean"]
 
 for TZ in all_TZs:
         print_TZ_statement(TZ = TZ)

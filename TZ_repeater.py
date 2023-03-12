5ce296cabaf21abced75f2258948df968223c3e3
Sobisonator <bomchasew@gmail.com>
2023-03-12 15:42:26 +0000

    Added new map mode view styles Tweaked areas

--- TZ_repeater.py
+++ TZ_repeater.py
@@ -1,15 +1,11 @@
-def print_TZ_statement(TZ):
+def print_TZ_statement(TZ, i):
 	loc = """	else_if = {{
-		limit = {{ var:trade_center = {{ has_variable = is_{TZ}_tradezone }} }}
-		SELL_set_revenue_cut_percentage = {{
-			tradegood = $tradegood$
-			tradezone = {TZ} # TODO: Normalise this so it doesn't need to take _tradezone suffix
-		}}
-		SELL_calculate_external_revenue_cut_percentage = {{
-			tradegood = $tradegood$
-			tradezone = {TZ}
-		}}
-	}}""".format(TZ=TZ)
+			limit = {{ {TZ}_tradezone = {{ PROVINCE = yes }} }}
+			MAPMODE_set_city_status = {{
+				mapmode_type = mapmode_colour
+				number = {i}
+			}}
+		}}""".format(TZ=TZ, i=i)
 	print(loc)
 
 #all_goods = ["grain","fur","industrial_fibres","textile_fibres","wool","silk","wood","stone","sulphur","whales","gems","peat","tin","inorganic_compounds","copper","iron","gold","silver","lead","coal","oil","tea","coffee","opium","tobacco","sugar","hardwood","rubber","dye","spices","temperate_fruit","tropical_fruit","mediterranean_fruit","chocolate","livestock","salt","fish","clothing","luxury_clothing","furniture","luxury_furniture","alcohol","glass","chemicals","rare_alloys","construction_materials","early_munitions","late_munitions","naval_supplies","steel_ships","wooden_ships","steel","bronze","machine_parts","early_artillery","late_artillery","electronics","pharmaceuticals","motors","processed_foods","petrochemicals"]
@@ -35,6 +31,7 @@ all_TZs = ["india",
            "baltic",
            "east_europe",
            "east_mediterranean"]
-
+i = 1
 for TZ in all_TZs:
-        print_TZ_statement(TZ = TZ)
+        print_TZ_statement(TZ = TZ, i=i)
+        i = i+1

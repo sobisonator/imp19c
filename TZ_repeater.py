a27b4ce88264ea4c9b90183aed96b53eb9506a42
Sobisonator <bomchasew@gmail.com>
2024-03-02 23:33:01 +0000

    Added sell declares to se_GLOBALTRADE

--- TZ_repeater.py
+++ TZ_repeater.py
@@ -1,17 +1,15 @@
 def print_TZ_statement(TZ):
-	loc = """	textbox = {{
-		parentanchor = top
-		position = {{ 85 73 }}
-		using = BaseFontM
-		using = Black_FontColor
-		fontsize = 16
-
-		visible = "[SimpleProvinceIcon.GetProvince.MakeScope.Var('is_{tradezone}_tradezone').IsSet]"
-
-		block = "title_text" {{
-			text = "#T [Player.MakeScope.Var('TZ_penetration_{tradezone}').GetValue|0%]"
+	loc = """svalue_shipping_{tradezone} = {{
+	# Scope: Country
+	# Function: Mitigate errors from missing shipping variables
+	value = 0
+	if = {{
+		limit = {{
+			has_variable = shipping_{tradezone}
 		}}
-	}}""".format(tradezone=TZ)
+		value = var:shipping_{tradezone}
+	}}
+}}""".format(tradezone=TZ)
 	print(loc)
 
 #all_goods = ["grain","fur","industrial_fibres","textile_fibres","wool","silk","wood","stone","sulphur","whales","gems","peat","tin","inorganic_compounds","copper","iron","gold","silver","lead","coal","oil","tea","coffee","opium","tobacco","sugar","hardwood","rubber","dye","spices","temperate_fruit","tropical_fruit","mediterranean_fruit","chocolate","livestock","salt","fish","clothing","luxury_clothing","furniture","luxury_furniture","alcohol","glass","chemicals","rare_alloys","construction_materials","early_munitions","late_munitions","naval_supplies","steel_ships","wooden_ships","steel","bronze","machine_parts","early_artillery","late_artillery","electronics","pharmaceuticals","motors","processed_foods","petrochemicals"]

16ab2af7e6b61fab427ba05a88616c7809b9f893
Sobisonator <bomchasew@gmail.com>
2024-03-24 14:50:48 +0000

    Implemented stockpile changes from exports and imports

--- TZ_repeater.py
+++ TZ_repeater.py
@@ -1,10 +1,12 @@
 def print_TZ_statement(TZ):
-	loc = """		TZ_is_{tradezone}_tradezone = {{
-			GT_get_governorship_amount_owed_tradegood = {{
-				tradegood = $tradegood$
-				tradezone = {tradezone}
-			}}
-		}}""".format(tradezone=TZ)
+	loc = """	set_global_variable = {{
+		name = {tradezone}_income_pool
+		value = global_payment_pool_$tradegood$
+	}}
+	change_global_variable = {{
+		name = {tradezone}_income_pool
+		multiply = {tradezone}_percentage_of_global_stockpile_$tradegood$
+	}}""".format(tradezone=TZ)
 	print(loc)
 
 #all_goods = ["grain","fur","industrial_fibres","textile_fibres","wool","silk","wood","stone","sulphur","whales","gems","peat","tin","inorganic_compounds","copper","iron","gold","silver","lead","coal","oil","tea","coffee","opium","tobacco","sugar","hardwood","rubber","dye","spices","temperate_fruit","tropical_fruit","mediterranean_fruit","chocolate","livestock","salt","fish","clothing","luxury_clothing","furniture","luxury_furniture","alcohol","glass","chemicals","rare_alloys","construction_materials","early_munitions","late_munitions","naval_supplies","steel_ships","wooden_ships","steel","bronze","machine_parts","early_artillery","late_artillery","electronics","pharmaceuticals","motors","processed_foods","petrochemicals"]
---
+++ descriptor.mod
@@ -0,0 +1,8 @@
+name="Imperatrix: Victoria"
+description=""
+tags={
+	"Overhaul"
+	"Total Conversions"
+}
+supported_version="2.0.3"
+path="G:/Documents/Paradox Interactive/Imperator/mod/imperatrix"

b4920049870a82b4969f7996fc8975aa4384940d
Sobisonator <bomchasew@gmail.com>
2024-08-07 22:20:49 +0100

    Minor GUI changes

--- TZ_repeater.py
+++ TZ_repeater.py
@@ -1,5 +1,5 @@
 def print_TZ_statement(TZ):
-	loc = """add = minimal_shipping_{tradezone}""".format(tradezone=TZ)
+	loc = """add = TZ_penetration_{tradezone}""".format(tradezone=TZ)
 	print(loc)
 
 #all_goods = ["grain","fur","industrial_fibres","textile_fibres","wool","silk","wood","stone","sulphur","whales","gems","peat","tin","inorganic_compounds","copper","iron","gold","silver","lead","coal","oil","tea","coffee","opium","tobacco","sugar","hardwood","rubber","dye","spices","temperate_fruit","tropical_fruit","mediterranean_fruit","chocolate","livestock","salt","fish","clothing","luxury_clothing","furniture","luxury_furniture","alcohol","glass","chemicals","rare_alloys","construction_materials","early_munitions","late_munitions","naval_supplies","steel_ships","wooden_ships","steel","bronze","machine_parts","early_artillery","late_artillery","electronics","pharmaceuticals","motors","processed_foods","petrochemicals"]

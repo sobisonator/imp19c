3770bce361bc73d74e2e8d19319de7a509f10e36
Sobisonator <bomchasew@gmail.com>
2025-03-25 23:51:28 +0000

    WiP AI interest rating and selection

--- TZ_repeater.py
+++ TZ_repeater.py
@@ -1,11 +1,12 @@
 def print_TZ_statement(TZ):
-	loc = """                      add = {{ 
-                                value = TZ_penetration_{tradezone}
-                                multiply = {{
-                                        value = global_var:global_{tradezone}_tradezone.var:TZ_shipping_capped
-                                        divide = 2000
-                                }}
-                        }}""".format(tradezone=TZ)
+	loc = """				else_if = {{
+					limit = {{
+						any_area_province = {{
+							{tradezone}_tradezone = {{ PROVINCE = yes }}
+						}}
+					}}
+					add = scope:AI_root.var:DIPLOMACY_power_{tradezone}
+				}}""".format(tradezone=TZ)
 	print(loc)
 
 #all_goods = ["grain","fur","industrial_fibres","textile_fibres","wool","silk","wood","stone","sulphur","whales","gems","peat","tin","inorganic_compounds","copper","iron","gold","silver","lead","coal","oil","tea","coffee","opium","tobacco","sugar","hardwood","rubber","dye","spices","temperate_fruit","tropical_fruit","mediterranean_fruit","chocolate","livestock","salt","fish","clothing","luxury_clothing","furniture","luxury_furniture","alcohol","glass","chemicals","rare_alloys","construction_materials","early_munitions","late_munitions","naval_supplies","steel_ships","wooden_ships","steel","bronze","machine_parts","early_artillery","late_artillery","electronics","pharmaceuticals","motors","processed_foods","petrochemicals"]

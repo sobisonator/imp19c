593384e3b7ad1ca741e9d1e7b51cb330a2ad8ac7
Sobisonator <battenberger1919@gmail.com>
2025-12-31 14:06:24 +0000

    Remove some obsolete files

--- map_data/state_writer.py
+++
@@ -1,4 +0,0 @@
-n = 2
-for n in range(2,797):
-    print("state_"+str(n))
-    n += 1
--- map_data/un-aread_prov_finder.py
+++
@@ -1,7 +0,0 @@
-import re
-
-with open("provs_without_areas.txt","r") as f:
-    lines = [line.rstrip('\n') for line in f]
-    for line in lines:
-        print(line[line.find("(")+len("("):line.rfind(")")])
-    
